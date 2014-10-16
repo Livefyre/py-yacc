cf0d44a9681897d045f7485fcb56b7c36cac2e7b
Andrew Thomson <athomsonguy@gmail.com>
2014-10-15 20:57:08 -0700

    Add output param to pyyacc.validate.

--- pyyacc/scripts/compile.py
+++ pyyacc/scripts/compile.py
@@ -12,7 +12,8 @@ import os
 import pickle
 import json
 from pyyacc.objects import ParseResult
-from safeoutput import SafeOutput
+from safeoutput import SafeOutputFile
+import hashlib
 
 
 def validate_main():
@@ -26,11 +27,17 @@ def validate_main():
                       help="Output format: yaml, json, sh, make are supported.")
     parser.add_option("-o", "--output", dest="output",
                       help="Output destination: path where to write output. If not provided, stdout is used.")
+    parser.add_option("-t", "--test", action='store_true',
+                      help="Tests to see if the contents in --output don't match whats specified by the yamls. Requires --output.")
 
     (options, yamls) = parser.parse_args()
     if not yamls:
         parser.print_usage()
         exit(-1)
+    if options.test and not options.output:
+        parser.print_usage()
+        exit(-1)
+
     b, params = build(*yamls)
     errs = b.validate(params, ignored=options.ignored.split(",") if options.ignored else [])
     if errs:
@@ -41,7 +48,7 @@ def validate_main():
         sys.exit(1)
         return
 
-    with SafeOutput(options.output) as output:
+    with SafeOutputFile(options.output) as output:
         if options.format == 'yaml':
             unparse(output, dict(params.iteritems()), default_flow_style=False)
         elif options.format == 'pickle':
@@ -70,10 +77,24 @@ def validate_main():
         else:
             print >> sys.stderr, "Invalid output format."
             sys.exit(2)
-    sys.exit(0)
+        if options.test:
+          output.flush()
+          same = _check_same(options.output, output.name)
+          if not same:
+            print >> sys.stderr, "Config mismatch!"
+            sys.exit(3)
 
 def _norm_sh_key(k):
     return k.upper().replace("-", "_")
+
+def _check_same(x, y):
+    with open(x, 'rb') as xfd:
+        xcontent = xfd.read()
+        xhash = hashlib.md5(xcontent).hexdigest()
+    with open(y, 'r') as yfd:
+        ycontent = yfd.read()
+        yhash = hashlib.md5(ycontent).hexdigest()
+    return xhash == yhash
 
 def sources_main():
     usage = "usage: %prog [options] yaml [yaml ...]"
