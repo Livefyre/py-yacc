f38c3fefe726c15c76dcd41dda19e815d6077e95
ninowalker <nino@livefyre.com>
2017-04-25 16:39:03 -0700

    !credential support.
    commit 23af326bdc2a864e764b232a33c510baadbbeda2
    Author: ninowalker <nino@livefyre.com>
    Date:   Tue Apr 25 13:37:42 2017 -0700
    
        Legacy testing
    
    commit fdcf23c647dfacd1614660a4a312133ce4b45435
    Author: ninowalker <nino@livefyre.com>
    Date:   Fri Apr 21 11:18:32 2017 -0700
    
        Fix logging; add determinism to key output.
    
    commit 7323d7618efeaa8862f8b69994f0f4c2f4484508
    Merge: cd80bd2 e3e158b
    Author: ninowalker <nino@livefyre.com>
    Date:   Fri Apr 21 11:06:20 2017 -0700
    
        Merge branch 'v2' into v2_creds
    
    commit e3e158bf79dea815c69b074e44d7e76fb2f35e83
    Author: ninowalker <nino@livefyre.com>
    Date:   Fri Apr 21 11:05:08 2017 -0700
    
        Adding circle.yml file
    
    commit cd80bd2ef5352a63746df2c94e99c3e27d66dc42
    Author: ninowalker <nino@livefyre.com>
    Date:   Fri Apr 21 10:29:54 2017 -0700
    
        Adding a custom type for credentials.
    
    commit d6985603ea877c237c25819e1f0ce23c16802fb0
    Author: ninowalker <nino@livefyre.com>
    Date:   Thu Apr 20 16:50:27 2017 -0700
    
        encourage use of pyyacc command.
    
    commit a10afe7ae19e4e2f30121bfe9226a5dbb36dc173
    Author: ninowalker <nino@livefyre.com>
    Date:   Thu Apr 20 16:36:54 2017 -0700
    
        Broad refactor and cleanup.

--- test/test_yml.py
+++ test/test_yml.py
@@ -1,25 +1,89 @@
 import StringIO
 import unittest
 
+from pyyacc.cstore import CredentialStore, EnvironmentResolver
 from pyyacc.yml import load
-from pyyacc.yml.extensions import Requirement, URI, ValueSpec, Optional
+from pyyacc.yml.extensions import Requirement, URI, ValueSpec, Optional,\
+    Credential
+import os
 
 
 parse = load
 
+
 class TestRegistration(unittest.TestCase):
     def test_required(self):
         load("!required cluster.yaml")
-        
+
+
 class TestParser(unittest.TestCase):
     def fd(self, string):
         return StringIO.StringIO(string)
-            
+
+    def test_parse_empty_cred(self):
+        x = parse(self.fd("""!credential"""))
+        assert isinstance(x, Credential)
+        self.assertEqual(x, "")
+
+    def test_parse_str_cred(self):
+        x = parse(self.fd("""!credential "abc" """))
+        assert isinstance(x, Credential)
+        self.assertEqual(x, "abc")
+
+    def test_parse_uri_cred(self):
+        x = parse(self.fd("""!credential "data:,abc" """))
+        assert isinstance(x, Credential)
+        self.assertEqual(x, "abc")
+
+    def test_parse_uri_cred2(self):
+        x = parse(self.fd("""!credential "data:application/vnd.pyyacc.credential,abc" """))
+        assert isinstance(x, Credential)
+        self.assertEqual(x, "abc")
+
+    def test_parse_uri_cred_b64(self):
+        x = parse(self.fd("""!credential "data:application/vnd.pyyacc.credential;base64,YWJj" """))
+        assert isinstance(x, Credential)
+        self.assertEqual(x, "abc")
+
+    def test_parse_uri_cred_param(self):
+        x = parse(self.fd("""!credential "data:application/vnd.pyyacc.credential;name=123;base64,YWJj" """))
+        assert isinstance(x, Credential)
+        self.assertEqual(x.fullstring(), 'data:application/vnd.pyyacc.credential;name=123;base64,YWJj')
+        self.assertEqual(x, "abc")
+        self.assertEqual(x.parameters, dict(name="123"))
+
+    def test_parse_uri_cred_params(self):
+        x = parse(self.fd(
+            """!credential "data:application/vnd.pyyacc.credential;name=123%3D;other=XXX;base64,YWJj" """))
+        assert isinstance(x, Credential)
+        self.assertEqual(x, "abc")
+        self.assertEqual(x.fullstring(), 'data:application/vnd.pyyacc.credential;name=123%3D;other=XXX;base64,YWJj')
+        self.assertEqual(x.parameters, dict(name="123=", other="XXX"))
+
+    def test_parse_cred_env_resolver(self):
+        os.environ['Unit_Test__key'] = 'meow'
+        CredentialStore.add_resolver(EnvironmentResolver())
+        x = parse(self.fd(
+            """!credential "data:application/vnd.pyyacc.credential;provider=Unit%20Test;name=key," """))
+        assert isinstance(x, Credential)
+        self.assertEqual(x.fullstring(), 'data:application/vnd.pyyacc.credential;name=key;provider=Unit%20Test,meow')
+        self.assertEqual(os.environ['Unit_Test__key'], "meow")
+        self.assertEqual(x, "meow")
+
+    def test_parse_cred_env_resolver_data_uri(self):
+        os.environ['Unit_Test__key'] = 'data:application/vnd.pyyacc.credential;base64,' + "meow".encode("base64")
+        CredentialStore.add_resolver(EnvironmentResolver())
+        x = parse(self.fd(
+            """!credential "data:application/vnd.pyyacc.credential;provider=Unit%20Test;name=key," """))
+        assert isinstance(x, Credential)
+        self.assertEqual(x.fullstring(), 'data:application/vnd.pyyacc.credential;name=key;provider=Unit%20Test,meow')
+        self.assertEqual(x, "meow")
+
     def test_parse_requirement(self):
         x = parse(self.fd("""!required "abc" """))
         assert isinstance(x, Requirement)
         repr(x)
-        
+
     def test_parse_optional(self):
         x = parse(self.fd("""!optional "abc" """))
         assert isinstance(x, Optional)
@@ -33,7 +97,7 @@ class TestParser(unittest.TestCase):
         assert x.hostname == "host"
         assert x.port == 25
         assert x.scheme == "smtp"
-        
+
     def test_parse_spec(self):
         x = parse(self.fd("""!spec\ntype: !!int "0"\ndescription:\nvalue: 1"""))
         assert isinstance(x, ValueSpec)
@@ -42,7 +106,7 @@ class TestParser(unittest.TestCase):
         assert not x.description
         assert not x.examples
         assert not x.deprecated
-        
+
         x = parse(self.fd("""!spec\ntype: !!int "0"\ndescription:\nvalue: 1\ndeprecated: true"""))
         assert x.deprecated
         repr(x)
